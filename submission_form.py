09969ba11493018b72cb487a394a2ae217675075
Jagadish Sivakumar <jagadish.sivakumar1@gmail.com>
2018-12-30 22:11:33 +0530

    Complete UI Form

--- submission_form.py
+++ submission_form.py
@@ -19,3 +19,10 @@ label_2.place(x=68,y=180)
 label_3=Label(root,text="Gender",width=20,font("bold",10))
 label_3.place(x=240,y=180)
 #Check box for Gender
+var = IntVar()
+Radiobutton(root.text="Male",padx=5,variable=var,value=1).place(x=235,y=230)
+Radiobutton(root.text="Female",padx=20,variable=var,value=2).place(x=290,y=230)
+
+#Submission Button
+Button(root,text='Submit',width=20,bg=20,bg='brown',fg='white').place(x=180,y=380)
+mainloop()
