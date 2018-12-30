f5bbe02192730aa6502eb295a97c50e62fb44f1c
Jagadish Sivakumar <jagadish.sivakumar1@gmail.com>
2018-12-30 22:06:42 +0530

    Created the labels and need to create the checkbox

--- submission_form.py
+++ submission_form.py
@@ -2,3 +2,20 @@ from tkinter import *
 root = Tk()
 root.geometry('500x500')
 root.title("Registration Form")
+
+#Heading
+label_0=Label(root, text="Registration form",width=20,font=("bold",20))
+label_0.place(x=90,y=53)
+
+#First Label
+label_1=Label(root,text="Full Name",width=20,font=("bold",10))
+label_1.place(x=80,y=130)
+
+#Second Label
+label_2=Label(root,text="E-mail",width=20,font=("bold",10))
+label_2.place(x=68,y=180)
+
+#Third Label
+label_3=Label(root,text="Gender",width=20,font("bold",10))
+label_3.place(x=240,y=180)
+#Check box for Gender
